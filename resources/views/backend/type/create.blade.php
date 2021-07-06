a01cb1e7e37be706eae763854bdac3bbc7d797d4
rifjan29 <Rifjan123>
2021-07-06 23:37:48 +0700

    Task

--- resources/views/backend/type/create.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.template')
-
-{{-- tambah data --}}
-@section('content')
-<x-breadcrumb title="Tambah Data Narator" :subtitle="Tambah Data" link="route('dashboard')" :linkBaru="$linkBaru" :subtitleBaru="$subtitleBaru"/>
-
-@endsection
